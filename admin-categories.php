2194f07d8e3379cbe595b5e6403d62b30c000fb2
brulogatti <bru.logatti@gmail.com>
2023-03-13 15:37:20 -0300

    Paginacao - Admin/Categorias

--- admin-categories.php
+++ admin-categories.php
@@ -8,12 +8,34 @@ use \Hcode\Model\Product;
 $app->get("/admin/categories", function(){
 	User::verifyLogin();
 
-	$categories = Category::listAll();
+	$search = (isset($_GET["search"])) ? $_GET["search"] : "";
+
+	$pg = (isset($_GET["page"])) ? (int)$_GET["page"]:1;
+
+	if($search != ""){
+		$pagination = Category::getPageSearch($search,$pg);
+	}else{
+		$pagination = Category::getPage($pg);
+	}
+
+	$pages = [];
+
+	for ($i=0; $i < $pagination["pages"]; $i++) { 
+		array_push($pages, [
+			"href"=>"/admin/categories?". http_build_query([
+				"page"=>$i+1,
+				"search"=>$search
+			]),
+			"text"=>$i+1
+		]);
+	}
 
 	$page = new PageAdmin();
 
 	$page->setTpl("categories", [
-		"categories"=>$categories
+		"categories"=>$pagination["data"],
+		"search"=>$search,
+		"pages"=>$pages
 	]);
 });
 
